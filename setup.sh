12a9b4b2567307df78ec00d9375c3691a214bc51
Azalinux <52433182+azalinux@users.noreply.github.com>
2022-05-04 22:00:04 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -110,15 +110,4 @@ sudo pacman -S --noconfirm --needed chromium
 sudo pacman -S --noconfirm --needed libreoffice-fresh
 sudo pacman -S --noconfirm --needed mpv
 
-
-
 sudo pacman -R --noconfirm epiphany
-
-
-sh trizen.sh
-
-trizen -S --noconfirm --needed --noedit pamac-aur
-trizen -S --noconfirm --needed --noedit realvnc-vnc-server
-trizen -S --noconfirm --needed --noedit realvnc-vnc-viewer
-#sudo /usr/bin/vnclicense -add P98BK-34AF4-V2K63-TMBH8-BYNRA
-sudo systemctl enable vncserver-x11-servicedd
