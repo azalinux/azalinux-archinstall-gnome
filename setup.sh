0a16c6cb5b21278769e147e4624ecb3a6045fdab
Azalinux <52433182+azalinux@users.noreply.github.com>
2022-05-04 11:36:45 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 set -e
 set -u
 
-#echo -n "127.0.0.1 localhost" >> /etc/hosts
-#echo -n "::1       localhost" >> /etc/hosts
-#echo -n "127.0.1.1 myhostname.localdomain myhostname" >> /etc/hosts
+echo -n "127.0.0.1 localhost" >> /etc/hosts
+echo -n "::1       localhost" >> /etc/hosts
+echo -n "127.0.1.1 myhostname.localdomain myhostname" >> /etc/hosts
 
 pacman -S --noconfirm --needed vi
-pacman -S --noconfirm --needed vim
+#pacman -S --noconfirm --needed vim
 
 # visudo [uncomment "Wheel root access"] (to save type: :wq )
 echo " visudo is about to run - uncomment the 'wheel root access' then type ':wq' to save and exit :)"
@@ -17,16 +17,16 @@ visudo
 sudo systemctl enable sshd
 
 sudo pacman -S --noconfirm --needed cups cups-pdf
-sudo pacman -S --noconfirm --needed ghostscript gsfonts gutenprint
-sudo pacman -S --noconfirm --needed gtk3-print-backends
-sudo pacman -S --noconfirm --needed libcups
+sudo pacman -S --noconfirm --needed gutenprint
+#sudo pacman -S --noconfirm --needed gtk3-print-backends
+#sudo pacman -S --noconfirm --needed libcups
 sudo pacman -S --noconfirm --needed system-config-printer
 
 sudo systemctl enable cups.service
 
 sudo pacman -S --noconfirm --needed firewalld
 sudo pacman -S --noconfirm --needed adobe-source-sans-pro-fonts
-sudo pacman -S --noconfirm --needed cantarell-fonts
+#sudo pacman -S --noconfirm --needed cantarell-fonts
 sudo pacman -S --noconfirm --needed noto-fonts
 sudo pacman -S --noconfirm --needed terminus-font
 sudo pacman -S --noconfirm --needed ttf-bitstream-vera
@@ -38,9 +38,9 @@ sudo pacman -S --noconfirm --needed ttf-roboto
 sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family
 sudo pacman -S --noconfirm --needed tamsyn-font
 
-sudo pacman -S --noconfirm --needed pipewire-pulse
-sudo pacman -S --noconfirm --needed bluez
-sudo pacman -S --noconfirm --needed bluez-libs
+#sudo pacman -S --noconfirm --needed pipewire-pulse
+#sudo pacman -S --noconfirm --needed bluez
+#sudo pacman -S --noconfirm --needed bluez-libs
 sudo pacman -S --noconfirm --needed bluez-utils
 sudo pacman -S --noconfirm --needed blueberry
 sudo systemctl --global enable pipewire-pulse
@@ -48,54 +48,51 @@ sudo systemctl --global enable pipewire-pulse
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 
-sudo pacman -S --noconfirm --needed baobab
-sudo pacman -S --noconfirm --needed accountsservice
+#sudo pacman -S --noconfirm --needed baobab
+#sudo pacman -S --noconfirm --needed accountsservice
 sudo pacman -S --noconfirm --needed dconf-editor
-sudo pacman -S --noconfirm --needed nano
+#sudo pacman -S --noconfirm --needed nano
 sudo pacman -S --noconfirm --needed traceroute
 
 sudo pacman -S --noconfirm --needed ffmpegthumbnailer
 #sudo pacman -S --noconfirm --needed gnome-disk-utility
-sudo pacman -S --noconfirm --needed gnome-keyring
+#sudo pacman -S --noconfirm --needed gnome-keyring
 #sudo pacman -S --noconfirm --needed gnome-system-monitor
 #sudo pacman -S --noconfirm --needed gnome-tweak-tool
 sudo pacman -S --noconfirm --needed gtk-engine-murrine
-sudo pacman -S --noconfirm --needed gvfs gvfs-mtp
+#sudo pacman -S --noconfirm --needed gvfs gvfs-mtp
 sudo pacman -S --noconfirm --needed kvantum-qt5
 #sudo pacman -S --noconfirm --needed kvantum-theme-arc
 sudo pacman -S --noconfirm --needed lsb-release
 sudo pacman -S --noconfirm --needed mlocate
 sudo pacman -S --noconfirm --needed polkit-gnome
 sudo pacman -S --noconfirm --needed qt5ct
-sudo pacman -S --noconfirm --needed sane
+#sudo pacman -S --noconfirm --needed sane
 sudo pacman -S --noconfirm --needed sysstat
 sudo pacman -S --noconfirm --needed screenfetch
 sudo pacman -S --noconfirm --needed nautilus-image-converter
 sudo pacman -S --noconfirm --needed ipset
 sudo pacman -S --noconfirm --needed dnsutils
 #sudo pacman -S --noconfirm --needed galculator
-sudo pacman -S --noconfirm --needed curl
+#sudo pacman -S --noconfirm --needed curl
 sudo pacman -S --noconfirm --needed dmidecode
 sudo pacman -S --noconfirm --needed glances
 sudo pacman -S --noconfirm --needed clamtk
 sudo pacman -S --noconfirm --needed brasero
-sudo pacman -S --noconfirm --needed sysstat
-sudo pacman -S --noconfirm --needed screenfetch
 sudo pacman -S --noconfirm --needed net-tools
-sudo pacman -S --noconfirm --needed lm_sensors
 sudo pacman -S --noconfirm --needed hexchat
 sudo pacman -S --noconfirm --needed gimp
 sudo pacman -S --noconfirm --needed p7zip
 sudo pacman -S --noconfirm --needed nfs-utils
 
-sudo pacman -S --noconfirm --needed gnome-font-viewer
+
 sudo pacman -S --noconfirm --needed grub-customizer 
 sudo pacman -S --noconfirm --needed inkscape
 sudo pacman -S --noconfirm --needed nomacs
 #sudo pacman -S --noconfirm --needed ristretto
-sudo pacman -S --noconfirm --needed wget
-sudo pacman -S --noconfirm --needed net-tools
-sudo pacman -S --noconfirm --needed htop
+
+
+
 sudo pacman -S --noconfirm --needed gtop
 sudo pacman -S --noconfirm --needed gparted
 sudo pacman -S --noconfirm --needed simplescreenrecorder
