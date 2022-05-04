ea7f9531d5b7c9dad90cb998fd444a72f162025b
Azalinux <52433182+azalinux@users.noreply.github.com>
2022-05-04 21:59:12 +1000

    Update trizen.sh

--- trizen.sh
+++ trizen.sh
@@ -6,3 +6,9 @@ set -u
 git clone https://aur.archlinux.org/trizen.git
 cd trizen
 makepkg -si --noconfirm
+
+trizen -S --noconfirm --needed --noedit pamac-aur
+trizen -S --noconfirm --needed --noedit realvnc-vnc-server
+trizen -S --noconfirm --needed --noedit realvnc-vnc-viewer
+#sudo /usr/bin/vnclicense -add P98BK-34AF4-V2K63-TMBH8-BYNRA
+sudo systemctl enable vncserver-x11-servicedd
