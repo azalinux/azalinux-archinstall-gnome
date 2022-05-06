b4449c14349d4fa112446c8c08f0b118e2907cbd
Azalinux <52433182+azalinux@users.noreply.github.com>
2022-05-06 11:30:40 +1000

    Update paru.sh

--- paru.sh
+++ paru.sh
@@ -8,8 +8,8 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 
-paru -S --noconfirm --needed --noedit pamac-aur
-paru -S --noconfirm --needed --noedit realvnc-vnc-server
-paru -S --noconfirm --needed --noedit realvnc-vnc-viewer
+paru -S --noconfirm --needed pamac-aur
+paru -S --noconfirm --needed realvnc-vnc-server
+paru -S --noconfirm --needed realvnc-vnc-viewer
 #sudo /usr/bin/vnclicense -add P98BK-34AF4-V2K63-TMBH8-BYNRA
 sudo systemctl enable vncserver-x11-servicedd
